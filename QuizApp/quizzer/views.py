cbfe98487937d66bf849d7d0fdb3a97adde8ce05
nishantsharma5592 <nishantsharma5592@gmail.com>
2024-02-21 21:16:33 +0530

    One page shows only one question
    However, cannot reset, to 0, only sequential clicking allowed.

--- QuizApp/quizzer/views.py
+++ QuizApp/quizzer/views.py
@@ -6,33 +6,44 @@ from django.template import loader
 from .models import Question
 # Create your views here.
 
+counter = 0
+correct = 0
+incorrect = 0
+score = 0
+limit = 2
+
 def index(request):
+    
+    global counter
+    global correct
+    global incorrect
+    global score
+    global limit
+
     template = loader.get_template("quizzer/index.html")
     
-    if request.method == 'POST':
-        counter = 1
-        questions=Question.objects.all()
-        correct = 0
-        incorrect = 0
-        score = 0
-        for q in questions:
-            # Since, all answers are received in this post request, we dedicate a counter
-            # to get choice for each question.
-            choice = request.POST.get ('choice '+str(counter))
-            print ('Choice question ', counter, ': ')
-            print (choice)
-            dict_ = vars(q) # This gives access to q
-            chosen = dict_.get(choice)
-            if q.correct_response == chosen:
-                correct += 1
-            else:
-                incorrect += 1
-            counter = counter + 1
-        score = correct
-        context = {'incorrect': incorrect, 'correct': correct, 'score':score}
-        return render(request, 'quizzer/results.html',  context)
+    # I want to make a change so that only one question is shown
 
+    if request.method == 'POST':
+        question = Question.objects.all()[counter]
+        choice = request.POST.get ('choice '+str(counter + 1))
+        print ('Choice question ', counter, ': ')
+        print (choice)
+        chosen = choice
+        if question.correct_response == chosen:
+            correct += 1
+        else:
+            incorrect += 1
+        counter += 1
+        if (counter == limit):
+            score = correct
+            context = {'incorrect': incorrect, 'correct': correct, 'score':score}
+            return render(request, 'quizzer/results.html',  context)
+        question = Question.objects.all()[counter]
+        context = {'question': question}
+        return HttpResponse(template.render(context, request))
     else:
-        questions = Question.objects.all()
-        context = {'questions': questions}
+        print ('Counter Here!: ', counter)
+        question = Question.objects.all()[counter]
+        context = {'question': question}
         return HttpResponse(template.render(context, request))
